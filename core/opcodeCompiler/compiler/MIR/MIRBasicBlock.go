7ba6bcc679f2e6b79a4967de53e6e1f183dfbff9
sunny2022da <124866865+sunny2022da@users.noreply.github.com>
2026-04-17 19:41:12 +0800

    debug(MIR): log CreatePhiMIR for block@3762 to find alternate PHI path
    Previous getEntryStackForBlock entry log never fired for block@3762 even
    though CFG dump shows 5 PHIs exist there. That means CreatePhiMIR is
    being called via a different code path (not getEntryStackForBlock).
    Add a one-line log inside CreatePhiMIR itself to see where and when the
    PHIs for this block actually get created.

--- core/opcodeCompiler/compiler/MIR/MIRBasicBlock.go
+++ core/opcodeCompiler/compiler/MIR/MIRBasicBlock.go
@@ -1,6 +1,7 @@
 package MIR
 
 import (
+	"github.com/ethereum/go-ethereum/log"
 	"github.com/holiman/uint256"
 )
 
@@ -765,6 +766,10 @@ func (b *MIRBasicBlock) CreatePhiMIR(ops []*Value, stack *ValueStack, phiStackIn
 	stack.push(mir.Result())
 	mir = b.appendMIR(mir)
 	mir.genStackDepth = stack.size()
+	if b != nil && b.firstPC == 3762 {
+		log.Warn("MIR CreatePhiMIR for block@3762",
+			"phiStackIndex", phiStackIndex, "numOps", len(ops))
+	}
 	return mir
 }
 
